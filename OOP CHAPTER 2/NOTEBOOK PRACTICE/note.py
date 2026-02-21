2c2daa8e3d474b10cc026b92e16c3aa9162fae19
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-02-21 17:22:39 +0500

    Added a file to store record for pages and its content

--- OOP CHAPTER 2/NOTEBOOK PRACTICE/note.py
+++ OOP CHAPTER 2/NOTEBOOK PRACTICE/note.py
@@ -6,10 +6,11 @@ from datetime import datetime
 
 class Note:
     """A class to represent notes"""
-
+    id = 0
     def __init__(self):
         """Initialize attributes"""
         self.content = ""
         self.time = datetime.now()
         self.tag = ""
-        self.writter = ""
+        self.writer = ""
+        Note.id += 1
--- OOP CHAPTER 2/NOTEBOOK PRACTICE/note_book.py
+++ OOP CHAPTER 2/NOTEBOOK PRACTICE/note_book.py
@@ -10,44 +10,53 @@ class NoteBook:
     def __init__(self):
         """Initialize notebook's attributes"""
         self.list_of_notes = {}
-        self.id = 1
+        self.file = "./OOP CHAPTER 2/NOTEBOOK PRACTICE/file.txt"
+        self.note = 0
 
-    def add(self):
+    def add(self: None) -> None:
         """Add new notes to notebook"""
-        note = Note()
+        self.note = Note()
         content = input("Add content: ")
-        note.content = content
+        self.note.content = content
         tag = input("Add tag: ")
-        note.tag = tag
-        writter = input("Name of the writter: ")
-        note.writter = writter
-        self.list_of_notes[self.id] = note
-        self.id += 1
+        self.note.tag = tag
+        writer = input("Name of the writter: ")
+        self.note.writer = writer
+        self._add_content_to_file(self.note)
+        self.list_of_notes[self.note.id] = self.note
 
-    def search(self):
+    def _add_content_to_file(self, note:Note) -> None:
+        """Adds content to the file with ID, owner, tag and content"""
+        with open(self.file, 'a') as file:
+                file.writelines("\n"+f"ID: {note.id}\nWritten by: {note.writer}\n"
+                                f"Tag: {note.tag}\n"
+                                f"Content: {note.content}")
+                return
+        print("The data cannot be written!")
+
+    def search(self: None) -> int:
         """Search for notes using tags"""
-        tag = input("Enter Tag name to search: ")
-        for key, value in self.list_of_notes.items():
-            if value.tag == tag:
-                print("Here is the following note: ")
-                print("Time: ", value.time)
-                print("Writter: ", value.writter)
-                print("Content: ", value.content)
-                return key, value
-        print("No Tag name found")
+        id = int(input("Enter ID to search: "))
+        if self.list_of_notes.get(id):
+            print("Here is the following note: ")
+            print("Time: ", self.list_of_notes[id].time)
+            print("Writer: ", self.list_of_notes[id].writer)
+            print("Content: ", self.list_of_notes[id].content)
+            return id
+        print("No ID found")
 
-    def modify(self):
+    def modify(self: None) -> None:
         """Modify existing notes"""
-        key, value = self.search()
+        key = self.search()
         change = input("now you can add more info to it: ")
-        value.content += change
-        self.list_of_notes[key] = value
+        self.list_of_notes[key].content += change
         print("Successfully added new data")
 
-    def delete(self):
+    def delete(self: None) -> None:
         """Delete notes from notebook"""
-        key, value = self.search()
-        print(f"This note was written by: {value.writter} at {value.time}")
+        key = self.search()
+        print(f"This note was written by: {self.list_of_notes[key].writer} "
+            f"at {self.list_of_notes[key].time}")
         delete = input("Write 'delete this note' to delete the note: ")
         if delete == 'delete this note':
             del self.list_of_notes[key]
